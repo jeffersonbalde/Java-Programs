ba86d2e03aeb01ec311a6f9d5e0ac78cd14c62b0
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-20 16:29:36 +0800

    negative to absolute number java program

---
+++ NegativeToAbsoluteNumber.java
@@ -0,0 +1,22 @@
+/*
+
+    Find the absolute number of the negative number
+
+ */
+
+import java.util.Scanner;
+
+public class NegativeToAbsoluteNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        
+        //Write your code here
+        if(num < 0) {
+            num = -num;
+        }
+        
+        System.out.println(num);
+    }
+}
