8c9b3e29a3d9d5313029af625285a69a936ae4c1
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-20 16:41:11 +0800

    even odd java program

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+/*
+
+    Log2Base Task:
+    Check if the input number is even or odd
+
+ */
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        //Write your code here
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+        
+    }    
+}
