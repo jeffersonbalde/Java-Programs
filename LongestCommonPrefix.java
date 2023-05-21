8eb389dc82a9bce12ae899f590f46df0eb7f4d53
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-21 14:20:12 +0800

    longest common prefix java program

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+/*
+
+    LeetCode Task:
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
+
+ */
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] arrTest = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(arrTest));
+        
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
