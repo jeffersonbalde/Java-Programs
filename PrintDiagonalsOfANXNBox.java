addf1dfa87a46505f84a0bbf6f98f36ed6b9782a
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-23 14:26:00 +0800

    print diagonals of a n x n box java program

---
+++ PrintDiagonalsOfANXNBox.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintDiagonalsOfANXNBox {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int i, j;
+    
+        //Write your code here
+        for(i = 1; i <= n; i++){
+            for(j = 1; j <= n; j++){
+                if(i == j || i + j == n+1) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
